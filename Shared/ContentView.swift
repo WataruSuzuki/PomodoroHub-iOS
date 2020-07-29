871c8afa292f65ba89a40428dd0dcf7f5b250b7f
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-07-29 22:30:00 +0900

    Created xcode project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 鈴木航 on 2020/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PomodoroHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroHubApp.swift
+//  Shared
+//
+//  Created by 鈴木航 on 2020/08/01.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
